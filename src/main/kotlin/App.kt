37b7a4b3f68123737c38b5443ede9dde3e538aa0
subroh0508 <subroh.0508@gmail.com>
2019-07-29 21:54:56 +0900

    [add]Add counter button

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,13 +1,38 @@
-import react.RBuilder
-import react.RComponent
-import react.RProps
-import react.RState
-import react.dom.div
 
-class App : RComponent<RProps, RState>() {
+import kotlinx.css.*
+import kotlinx.html.js.onClickFunction
+import react.*
+import react.dom.div
+import styled.css
+import styled.styledButton
+
+external interface AppState : RState {
+    var count: Int
+}
+
+class App : RComponent<RProps, AppState>() {
+    override fun AppState.init() {
+        count = 0
+    }
+
+    private fun onClickedButton() {
+        setState { count += 1 }
+    }
+
     override fun RBuilder.render() {
-        div {
-            +"Hello, World!"
+        div { +"count: ${state.count}" }
+
+        styledButton {
+            css {
+                width = 200.px
+                height = 100.px
+
+                backgroundColor = Color.aqua
+            }
+
+            attrs.onClickFunction = { onClickedButton() }
+
+            +"click!"
         }
     }
 }
